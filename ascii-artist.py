8f7c5d92ada6f9dea6d6725e1a7c053ca9c7edb6
capatika <kisagardi@gmail.com>
2020-06-05 17:52:39 +0200

    added dark mode feature

--- ascii-artist.py
+++ ascii-artist.py
@@ -3,8 +3,9 @@ from statistics import mean
 import argparse
 
 parser = argparse.ArgumentParser(description="Converts an image to ASCII characters.")
+parser.add_argument('--foreach', dest='foreach', action='store_true', default=False, help="converts *each* pixel to an ASCII character")
+parser.add_argument('--dark', dest='dark', action='store_true', default=False, help="creates the image with dark background")
 parser.add_argument('file', help="the original file")
-parser.add_argument('--foreach', dest='foreach', action='store_true', default=False, help="converts each pixel to an ASCII character")
 args = parser.parse_args()
 
 horizontal = 6
@@ -15,13 +16,15 @@ if not args.foreach:
     width = int(original.size[0] / horizontal)
     height = int(original.size[1] / vertical)
 else: width, height = original.size[0], original.size[1]
-new = Image.new('L', (width * horizontal, height * vertical), color=255)
+if not args.dark: new = Image.new('L', (width * horizontal, height * vertical), color=255)
+else: new = Image.new('L', (width * horizontal, height * vertical), color=0)
 draw = ImageDraw.Draw(new)
 font = ImageFont.truetype('consola.ttf', 11)
 out_text = ''
 
-chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-converter = 69 / 255
+if not args.dark: chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+else: chars = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+converter = (len(chars) - 1) / 255
 
 if not args.foreach:
     for row in range(height):
@@ -38,5 +41,6 @@ else:
             out_text += chars[round(original.getpixel((x, y)) * converter)]
         out_text += '\n'
 
-draw.text((0, 0), out_text, font=font, fill=0, spacing=1)
+if not args.dark: draw.text((0, 0), out_text, font=font, fill=0, spacing=1)
+else: draw.text((0, 0), out_text, font=font, fill=255, spacing=1)
 new.save(args.file + '-ascii.png')
